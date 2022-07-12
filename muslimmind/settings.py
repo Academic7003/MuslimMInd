a48c55a32186e2ea6d661021da75af6c68f25ad9
Academic7003 <nazarmamatovnurmuhammad@gmail.com>
2022-07-12 16:42:26 +0500

    deploy

--- muslimmind/settings.py
+++ muslimmind/settings.py
@@ -120,14 +120,14 @@ USE_TZ = True
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'media'),
+
 ]
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
