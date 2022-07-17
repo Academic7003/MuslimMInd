a65b2e4bd89ca5b0d8fcc0d6dcf65dca3487f880
Academic7003 <nazarmamatovnurmuhammad@gmail.com>
2022-07-17 15:08:29 +0500

    heroku

--- muslimmind/wsgi.py
+++ muslimmind/wsgi.py
@@ -1,4 +1,5 @@
 
+from whitenoise import WhiteNoise
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -6,3 +7,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'muslimmind.settings')
 
 application = get_wsgi_application()
+
+application = WhiteNoise(application, root="static")
+# application.add_files("/static/")
+#  prefix="more-files/"
